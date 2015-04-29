9811f3f849399b7159544a9499b99294759388d6
Byron (GU Yu) <ygu@thoughtworks.com>
2015-04-29 15:44:23 +1000

    change docker registry variabel value

--- utils/registry/setup.sh
+++ utils/registry/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 which docker
 if [ "$?" -eq 0 ]; then
-	docker run --name registry -d -e STORAGE_PATH=/home/vagarnt/share/registry -p 5000:5000 registry
+	docker run --name registry -d -p 5000:5000 registry
 else
 	exit 1
 fi
