df17610ddc6a674e698f6e7d985a26ec5c6a4987
Byron (GU Yu) <ygu@thoughtworks.com>
2015-05-07 16:57:23 +1000

    Remove prompts

--- utils/shipyard/setup.sh
+++ utils/shipyard/setup.sh
@@ -8,6 +8,5 @@ if [ "$?" -eq 0 ]; then
 		docker run -d -v /var/run/docker.sock:/var/run/docker.sock shipyard/deploy start
 	done
 else
-	echo "Docker should be installed and in right privilledges."
 	exit 1
 fi
