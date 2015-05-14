59cc2e1939949a3b10e0f2359bf71e097f791d6e
Byron (GU Yu) <ygu@thoughtworks.com>
2015-05-14 11:32:05 +1000

    Use docker-compose

--- provision.sh
+++ provision.sh
@@ -3,6 +3,7 @@ locale-gen
 sh -c "echo 'LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8' >> /etc/environment"
 apt-get update -y
 apt-get upgrade -y
+apt-get install -y python-pip
 
 #Install docker
 docker --version | grep Docker
@@ -17,9 +18,14 @@ else
 	sudo usermod -aG docker vagrant
 fi
 
+sudo pip install -U docker-compose
+
+cd /home/vagrant/utils/
+docker-compose up -d
+
 #Install services
-sh -c /home/vagrant/utils/jenkins/setup_docker.sh
-sh -c /home/vagrant/utils/gogs/setup.sh
-sh -c /home/vagrant/utils/registry/setup.sh
-sh -c /home/vagrant/utils/dockerboard/setup.sh
-sh -c /home/vagrant/utils/taskboard/setup.sh
+# sh -c /home/vagrant/utils/jenkins/setup_docker.sh
+# sh -c /home/vagrant/utils/gogs/setup.sh
+# sh -c /home/vagrant/utils/registry/setup.sh
+# sh -c /home/vagrant/utils/dockerboard/setup.sh
+# sh -c /home/vagrant/utils/taskboard/setup.sh
