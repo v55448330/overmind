4cd59fde9d53d94de9d0d1962b513ddc5d11d35d
Byron (GU Yu) <ygu@thoughtworks.com>
2015-05-20 23:57:36 +1000

    Update provision.sh

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,7 +3,8 @@ locale-gen
 sh -c "echo 'LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8' >> /etc/environment"
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y python-pip git
+apt-get install -y git python-setuptools
+easy_install -U pip
 
 #Install docker
 docker --version | grep Docker
@@ -20,6 +21,8 @@ fi
 
 sudo pip install -U docker-compose
 
+su - vagrant
+rm -rf overmind
 git clone https://github.com/wizardbyron/overmind.git
 cd overmind
 docker-compose up -d
